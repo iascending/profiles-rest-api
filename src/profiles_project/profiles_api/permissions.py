8dabf4425197ca635fc766939e40bb1075577c5e
Jason <iascending@gmail.com>
2018-12-30 10:27:57 +1100

    Added improved permissions to use profile feed item API

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         # print(permissions.SAFE_METHODS)
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+        """Allow users to update their own status."""
+
+        def has_object_permission(self, request, view, obj):
+            """Check user is trying to update their own status."""
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            # print(permissions.SAFE_METHODS)
+            return obj.user_profile.id == request.user.id
